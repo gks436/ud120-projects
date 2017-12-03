ddf04a906565fe9aab949267dbe5a8b92f68279b
gks436 <30575237+gks436@users.noreply.github.com>
2017-12-03 14:23:29 +0530

    Update k_means_cluster.py

--- k_means/k_means_cluster.py
+++ k_means/k_means_cluster.py
@@ -65,9 +65,9 @@ plt.show()
 
 ### cluster here; create predictions of the cluster labels
 ### for the data and store them to a list called pred
-kmeans=KMeans(n_clusters=2).fit(finance_features)
-kmeans.labels_
-pred=kmeans.predict(finance_1,finance_2)
+kmeans=KMeans(n_clusters=2)
+kmeans.fit(finance_features)
+pred=kmeans.labels_
 
 
 
